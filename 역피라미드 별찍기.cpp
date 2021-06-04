e3fb2e2d8ac1827994c6d0acfd447433d926075d
jun0-258 <84830670+jun0-258@users.noreply.github.com>
2021-06-04 21:21:29 +0900

    Add files via upload

---
+++ 역피라미드 별찍기.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main( ) {
+	int num=5;
+for(int i=0;i<num;i++){
+    for(int j=0;j<i;j++){
+         printf(" ");
+     }
+
+    for(int j=2*num-1;j>2*i;j--){
+	printf("*");
+    }
+    printf("\n");
+}
+
+}
