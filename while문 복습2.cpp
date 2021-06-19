86ac361ae05b9d9235d1b83d0c7313bb3e841a19
jun0-258 <84830670+jun0-258@users.noreply.github.com>
2021-06-19 23:55:11 +0900

    Add files via upload

---
+++ while문 복습2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main( ) {
+	int i = 0, hap = 0;
+	while(1){
+		i++;
+		if(i > 10)
+		  break;
+		if(i % 5 == 0)
+		  continue;
+		hap += i;
+	}
+	printf("i = %d, hap = %d \n", i, hap);
+}
